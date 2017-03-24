8d5eb46b0c6778bc95d86b4fd7c85de19e84a6d5
Arash Outadi <arash.out@gmail.com>
2017-03-24 12:16:33 -0700

    Python program produces JS Sudoku grids

--- convertTxtToArr2d.py
+++ convertTxtToArr2d.py
@@ -1,21 +1,33 @@
 def convertTxtGridToJSArr(listLines, index, n):
     index += 1
-    jsArray = ""
-    for i in range(0,n):
-        row = listLines[i+index]
-        for c in row:
-            pass
-
+    jsArray = "[\n"
+    for r in range(0, n):
+        row = listLines[r + index]
+        for c in range(0, n):
+            if c == (n - 1):
+                jsArray += "{0}],\n".format(row[c])
+            elif c == 0:
+                jsArray += "[{0},".format(row[c])
+            else:
+                jsArray += "{0},".format(row[c])
+    jsArray += "]\n"
+    return jsArray
 
 filename = "sudoku.txt"
+jsFilename = "jsSudoku.txt"
 
 with open(filename, "r") as f:
     listLines = f.readlines()
 
 n = 9
+jsText = ""
 for i in range(0, len(listLines)):
     line = listLines[i]
     # Start of a grid
     if "Grid" in line:
-        convertTxtGridToJSArr(listLines, i, n)
+        jsText += line + "\n"
+        jsText += convertTxtGridToJSArr(listLines, i, n)
         break
+
+with open(jsFilename, "w") as f:
+    f.write(jsText)
